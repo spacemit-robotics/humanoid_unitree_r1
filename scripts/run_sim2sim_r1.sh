1afcc37692ab2713be592db1b76814c7c9630d65
beililcj <beililcj@163.com>
2026-04-29 21:42:43 +0800

    feat(humanoid_unitree_r1): initial release
    Signed-off-by: beililcj <beililcj@163.com>

---
+++ scripts/run_sim2sim_r1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+: "${SDK_ROOT:=$(cd "$(dirname "$(readlink -f "$0")")/../../.." && pwd)}"
+exec control_sim2sim_runtime "$SDK_ROOT/application/native/humanoid_unitree_r1/config/r1.yaml"
